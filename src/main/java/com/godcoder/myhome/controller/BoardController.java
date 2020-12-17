b62cc90d4efa710422da0908de00f2a03a42ed62
김소연 <o2o202@nate.com>
2020-12-17 15:43:33 +0900

    검색 및 페이징처리, Spring Security 로그인, @OneToMany 관계 설정

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -3,7 +3,13 @@ package com.godcoder.myhome.controller;
 import com.godcoder.myhome.Validator.BoardValidator;
 import com.godcoder.myhome.model.Board;
 import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,12 +25,21 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
     @Autowired
+    private BoardService boardService;
+    @Autowired
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model){
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText){
+       // Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContent(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() -4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() +4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
+
         return "board/list";
     }
 
@@ -41,12 +56,14 @@ public class BoardController {
 
     //BindingResult는 model에서 설정해놓은 어노테이션의 조건
     @PostMapping("/form")
-    public String greetingSubmit(@Valid Board board, BindingResult bindingResult){
+    public String postForm(@Valid Board board, BindingResult bindingResult, Authentication authentication){
         boardValidator.validate(board, bindingResult);
         if(bindingResult.hasErrors()){
             return "board/form";
         }
-        boardRepository.save(board);
+        String username = authentication.getName();
+        boardService.save(username, board);
+       // boardRepository.save(board);
         return "redirect:/board/list";
     }
 
